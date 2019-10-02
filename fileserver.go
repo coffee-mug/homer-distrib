dab1a013af9cbcce85e2ae27a1cf6837ddfca50b
cofee-mug <lucas.kostka@escdijon.eu>
2019-10-03 00:10:18 +0200

    Feature: Upload a text file

--- fileserver.go
+++ fileserver.go
@@ -1,7 +1,9 @@
 package fileserver
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -13,8 +15,20 @@ func FileServer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if strings.HasPrefix(r.URL.Path, "/files/") {
-		filename := r.URL.Path[len("/files/"):]
+		if r.Method == "POST" {
+			buf := bytes.Buffer{}
 
-		http.ServeFile(w, r, filename)
+			buf.ReadFrom(r.Body)
+
+			// TODO: hardcoded name for now, to update later
+			// with more tests
+			ioutil.WriteFile("test.txt", buf.Bytes(), 0644)
+
+			fmt.Fprintf(w, buf.String())
+		} else {
+			filename := r.URL.Path[len("/files/"):]
+
+			http.ServeFile(w, r, filename)
+		}
 	}
 }
--- fileserver_test.go
+++ fileserver_test.go
@@ -1,10 +1,22 @@
 package fileserver
 
 import (
+	"bytes"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+// utils
+func assertResponseBody(t *testing.T, response *httptest.ResponseRecorder, expected string) {
+	got := response.Body.String()
+
+	if got != expected {
+		t.Errorf("Error: got %s want %s", got, expected)
+	}
+}
 
 func TestFileServer(t *testing.T) {
 	t.Run("GET /echo/$message should return message", func(t *testing.T) {
@@ -14,11 +26,6 @@ func TestFileServer(t *testing.T) {
 
 		FileServer(response, request)
 
-		got := response.Body.String()
-
-		if got != message {
-			t.Errorf("Error: got %s want %s", got, message)
-		}
 	})
 
 	t.Run("GET /files/$file.txt should return the content of the file", func(t *testing.T) {
@@ -30,11 +37,32 @@ func TestFileServer(t *testing.T) {
 
 		FileServer(response, request)
 
-		got := response.Body.String()
+		assertResponseBody(t, response, expected)
+	})
 
-		if got != expected {
-			t.Errorf("Error: got %s want %s", got, expected)
+	t.Run("POST /files should upload the file, with filename test.txt, to the server", func(t *testing.T) {
+		fileName := "test.txt"
+		// TODO: push that test further to mimick command line file arguments
+		fileContent, err := ioutil.ReadFile("input_test_file.txt")
+		if err != nil {
+			log.Fatal("Could not open input_test_file.txt")
 		}
 
+		request, _ := http.NewRequest(http.MethodPost, "/files/", ioutil.NopCloser(bytes.NewReader(fileContent)))
+		request.Header.Set("Content-Type", "text/plain")
+
+		response := httptest.NewRecorder()
+
+		FileServer(response, request)
+
+		assertResponseBody(t, response, string(fileContent))
+
+		// See if the file has been correctly saved
+		request, _ = http.NewRequest(http.MethodGet, "/files/"+fileName, nil)
+		response = httptest.NewRecorder()
+
+		FileServer(response, request)
+
+		assertResponseBody(t, response, string(fileContent))
 	})
 }
